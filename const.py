4bfc2574184b42fb054a63980a85600275d9fa24
Johann <mail@byjohan.eu>
2024-04-04 01:18:02 +0200

    Delete const.py

--- const.py
+++
@@ -1,9 +0,0 @@
-"""Constants for the ADHAN integration."""
-
-import homeassistant.helpers.config_validation as cv
-from homeassistant.const import CONF_RESOURCES
-
-DOMAIN = "adhan"
-
-CONF_COUNTRY = "NL"
-CONF_CITY = "BREUKELEN"
