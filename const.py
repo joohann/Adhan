b62aaea69008e6219fb43febc6204461dffb6245
Johann <mail@byjohan.eu>
2024-04-04 01:04:35 +0200

    Update const.py

--- const.py
+++ const.py
@@ -1,5 +1,8 @@
 """Constants for the ADHAN integration."""
 
+import homeassistant.helpers.config_validation as cv
+from homeassistant.const import CONF_RESOURCES
+
 DOMAIN = "adhan"
 
 CONF_COUNTRY = "NL"
