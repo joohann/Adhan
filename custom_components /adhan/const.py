5a422c63d221174d54256e3139eeff72b27b6e09
Johann <mail@byjohan.eu>
2024-04-04 01:20:24 +0200

    Create const.py

---
+++ custom_components /adhan/const.py
@@ -0,0 +1,9 @@
+"""Constants for the ADHAN integration."""
+
+import homeassistant.helpers.config_validation as cv
+from homeassistant.const import CONF_RESOURCES
+
+DOMAIN = "adhan"
+
+CONF_COUNTRY = "NL"
+CONF_CITY = "BREUKELEN"
