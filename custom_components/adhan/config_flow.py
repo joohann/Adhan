11125b1fcabf32f4be3be7b919d8eeecd1dc9e44
Johann <mail@byjohan.eu>
2024-04-16 20:49:55 +0200

    Update config_flow.py

--- custom_components/adhan/config_flow.py
+++ custom_components/adhan/config_flow.py
@@ -1,13 +1,6 @@
-"""Config flow for the Adhan component."""
 import secrets
-
 from homeassistant import config_entries
-
-from .const import (
-    DOMAIN,
-    NAME,
-)
-
+from .const import DOMAIN, NAME
 
 class AdhanConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Config flow for Adhan."""
@@ -17,6 +10,18 @@ class AdhanConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         """Handle a flow initialized by the user."""
+        
+        errors = {}
+
+        if user_input is not None:
+            city = user_input.get("city")
+            country = user_input.get("country")
+            
+            # Save the entered city and country in configuration file
+            self._async_abort_entries_match({"city": city, "country": country})
+
+            # Proceed to create the entry
+            return self.async_create_entry(title=NAME, data={"city": city, "country": country})
 
         # Only a single instance of the integration
         if self._async_current_entries():
@@ -25,6 +30,16 @@ class AdhanConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         id = secrets.token_hex(6)
 
         await self.async_set_unique_id(id)
-        self._abort_if_unique_id_configured(updates=user_input)
+        self._abort_if_unique_id_configured()
 
-        return self.async_create_entry(title=NAME, data={})
+        # Display the form for the user to input city and country
+        return self.async_show_form(
+            step_id="user",
+            data_schema=config_entries.CONFIG_SCHEMA.extend(
+                {
+                    vol.Required("city"): str,
+                    vol.Required("country"): str,
+                }
+            ),
+            errors=errors,
+        )
