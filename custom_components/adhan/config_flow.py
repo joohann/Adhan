3951ab580ecb42b2264346ce0ab00abc33c28a83
Johann <joohann@icloud.com>
2025-03-08 02:19:25 +0100

    Update config_flow.py

--- custom_components/adhan/config_flow.py
+++ custom_components/adhan/config_flow.py
@@ -1,46 +1,29 @@
-import secrets
-import voluptuous as vol
 from homeassistant import config_entries
-from .const import DOMAIN, NAME
+import voluptuous as vol
 
-class AdhanConfigFlow(config_entries.ConfigFlow):
-    """Config flow for Adhan."""
+from .const import DOMAIN, CONF_CITY, CONF_COUNTRY, CONF_METHOD
 
-    VERSION = "1.0.0"
-    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
+class AdhanConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Adhan."""
+
+    VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        """Handle a flow initialized by the user."""
-        
+        """Handle the initial step."""
         errors = {}
-
+        
         if user_input is not None:
-            city = user_input.get("city")
-            country = user_input.get("country")
-            
-            # Save the entered city and country in configuration file
-            self._async_abort_entries_match({"city": city, "country": country})
+            # Controleer of alle invoer geldig is
+            if not user_input[CONF_CITY] or not user_input[CONF_COUNTRY]:
+                errors["base"] = "invalid_location"
+            else:
+                # Sla de configuratie op
+                return self.async_create_entry(title="Adhan", data=user_input)
 
-            # Proceed to create the entry
-            return self.async_create_entry(title=NAME, data={"city": city, "country": country})
-
-        # Only a single instance of the integration
-        if self._async_current_entries():
-            return self.async_abort(reason="single_instance_allowed")
-
-        id = secrets.token_hex(6)
-
-        await self.async_set_unique_id(id)
-        self._abort_if_unique_id_configured()
-
-        # Display the form for the user to input city and country
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema(
-                {
-                    vol.Required("city"): str,
-                    vol.Required("country"): str,
-                }
-            ),
-            errors=errors,
-        )
+        # Formulier voor gebruiker
+        schema = vol.Schema({
+            vol.Required(CONF_CITY, default="Breukelen"): str,
+            vol.Required(CONF_COUNTRY, default="Netherlands"): str,
+            vol.Optional(CONF_METHOD, default="3"): str,
+        })
+        return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
