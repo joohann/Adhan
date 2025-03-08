5ca7ecf00724c486aeec38c8c908dbf058333abf
Johann <joohann@icloud.com>
2025-03-08 02:15:23 +0100

    Update __init__.py

--- custom_components/adhan/__init__.py
+++ custom_components/adhan/__init__.py
@@ -1,29 +1,21 @@
-"""The Adhan integration."""
-
-
-import logging
-from datetime import datetime
-from datetime import timedelta
-
-from homeassistant.const import Platform
-
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.typing import ConfigType
+from homeassistant.config_entries import ConfigEntry
+
 from .const import DOMAIN
 
-async def async_setup(hass, config):
-    """Set up the Adhan component."""
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the Adhan integration."""
+    hass.data[DOMAIN] = {}
     return True
 
-async def async_setup_entry(hass, entry):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up Adhan from a config entry."""
-    hass.data.setdefault(DOMAIN, {})
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, 'sensor')
-    )
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+    hass.async_create_task(hass.config_entries.async_forward_entry_setup(entry, "sensor"))
     return True
 
-async def async_unload_entry(hass, entry):
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload a config entry."""
-    await hass.config_entries.async_forward_entry_unload(entry, 'sensor')
+    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    hass.data[DOMAIN].pop(entry.entry_id)
     return True
